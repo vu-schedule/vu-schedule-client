d88698bfe7b2d7ad6f27cb9586f8217c3d0fef91
Dainius Kavoliunas <dainius.kavoliunas@gmail.com>
2018-01-26 01:34:53 +0100

    Github link

--- src/theme/AppFooter.vue
+++ src/theme/AppFooter.vue
@@ -5,6 +5,10 @@
         Atsiliepimai ir komentarai
         <a href="mailto:dainius.kavoliunas@gmail.com">dainius.kavoliunas@gmail.com</a>
       </div>
+        <div class="content has-text-centered">
+        Source kodas
+        <a href="https://github.com/vu-schedule" target="_blank">Github</a>
+      </div>
     </div>
   </footer>
 </template>
